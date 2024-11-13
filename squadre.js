d264b4102a1adaba8e32488b55aa8cc7d1eb05e8
Simone-Fratini <simone@fratini.com>
2024-11-13 17:11:57 +0100

    database squadre

---
+++ squadre.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        nome: 'squadra1',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra2',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra3',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra4',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra5',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra6',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra7',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra8',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra9',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'squadra10',
+        punti: 0,
+        falli: 0
+    }
+]
