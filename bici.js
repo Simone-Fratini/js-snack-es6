c610a749b29468a023d72060ef1d875aafe12e03
Simone-Fratini <simone@fratini.com>
2024-11-13 17:09:38 +0100

    bici database

---
+++ bici.js
@@ -0,0 +1,129 @@
+export default [
+    {
+        nome: 'bici15',
+        peso: 80
+    },
+    {
+        nome: 'bici3',
+        peso: 20
+    },
+    {
+        nome: 'bici27',
+        peso: 140
+    },
+    {
+        nome: 'bici1',
+        peso: 10
+    },
+    {
+        nome: 'bici21',
+        peso: 110
+    },
+    {
+        nome: 'bici9',
+        peso: 50
+    },
+    {
+        nome: 'bici5',
+        peso: 30
+    },
+    {
+        nome: 'bici19',
+        peso: 100
+    },
+    {
+        nome: 'bici13',
+        peso: 70
+    },
+    {
+        nome: 'bici25',
+        peso: 130
+    },
+    {
+        nome: 'bici7',
+        peso: 40
+    },
+    {
+        nome: 'bici31',
+        peso: 160
+    },
+    {
+        nome: 'bici11',
+        peso: 60
+    },
+    {
+        nome: 'bici23',
+        peso: 120
+    },
+    {
+        nome: 'bici17',
+        peso: 90
+    },
+    {
+        nome: 'bici29',
+        peso: 150
+    },
+    {
+        nome: 'bici6',
+        peso: 35
+    },
+    {
+        nome: 'bici2',
+        peso: 15
+    },
+    {
+        nome: 'bici20',
+        peso: 105
+    },
+    {
+        nome: 'bici8',
+        peso: 45
+    },
+    {
+        nome: 'bici4',
+        peso: 25
+    },
+    {
+        nome: 'bici32',
+        peso: 165
+    },
+    {
+        nome: 'bici14',
+        peso: 75
+    },
+    {
+        nome: 'bici10',
+        peso: 55
+    },
+    {
+        nome: 'bici18',
+        peso: 95
+    },
+    {
+        nome: 'bici24',
+        peso: 125
+    },
+    {
+        nome: 'bici12',
+        peso: 65
+    },
+    {
+        nome: 'bici30',
+        peso: 155
+    },
+    {
+        nome: 'bici26',
+        peso: 135
+    },
+    {
+        nome: 'bici22',
+        peso: 115
+    },
+    {
+        nome: 'bici28',
+        peso: 145
+    },
+    {
+        nome: 'bici16',
+        peso: 85
+    }];
